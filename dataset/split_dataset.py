aa3df5896dee7cb1f5e7b7e3f84d0b46f500b341
Nathan Trugilho Braga <trugicraft@gmail.com>
2025-09-22 17:15:51 -0300

    criei um código para monitorar o mse ao longo das iterações

--- dataset/split_dataset.py
+++ dataset/split_dataset.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
-SEED = 42 # semente para caso eu precise embaralhar dnv com a mesma divisão
+SEED = 42 # Guardo a semente do shuffle
 
-df = pd.read_csv("student-por.csv")
+# Carregar dataset
+df = pd.read_csv("dataset/student-por.csv")
+
+# Mapear variáveis binárias 'yes'/'no' para 1/0
+binarias = ["higher", "internet", "romantic"]
+df[binarias] = df[binarias].applymap(lambda x: 1 if x == "yes" else 0)
+
+# Selecionar atributos e target
+cols = ["G2", "G1", "failures", "studytime", "Medu", "Fedu", 
+        "higher", "internet", "romantic", "G3"]
+df = df[cols]
 
 # Embaralhar instâncias
 df_shuffled = df.sample(frac=1, random_state=SEED).reset_index(drop=True)
@@ -12,6 +22,5 @@ train_size = int(0.8 * len(df_shuffled))
 train_df = df_shuffled.iloc[:train_size]
 test_df = df_shuffled.iloc[train_size:]
 
-# Salvar em arquivos
 train_df.to_csv("train.csv", index=False)
 test_df.to_csv("test.csv", index=False)
---
+++ mse_evolution/mse_evo.py
@@ -0,0 +1,69 @@
+from pysr import PySRRegressor  
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+ARQUIVO_TREINO = "dataset/train.csv"
+ARQUIVO_TESTE = "dataset/test.csv"
+
+# Carrego toda a planilha
+dados_planilha = np.loadtxt(ARQUIVO_TREINO, delimiter=',', dtype=float, skiprows=1)
+
+# Última coluna (G3) como var dependente
+var_dependentes = dados_planilha[:, -1]  
+
+# Todas as outras como vars independentes
+var_independentes = dados_planilha[:, :-1]  
+
+
+# Carrego os dados da planilha para a memória ==================
+dados_planilha = np.loadtxt(ARQUIVO_TESTE, delimiter=',', dtype=float, skiprows=1)
+
+# Faço a divisão entre as variáveis dependentes e independentes 
+var_dep = dados_planilha[ : , -1] # Contém somente Y
+var_ind = dados_planilha[:, :-1]    # Contém somente X                        
+
+# Definindo meu modelo ===================
+modelo = PySRRegressor(
+    model_selection="best",
+    niterations=1,  # Botei um número bem alto para a condição de parada ser dada apenas pelo tempo
+    populations=1, # Default = 15
+    population_size=100, # Default = 33
+    maxsize=30, # Limita a complexidade máxima das equações (Default = 20)
+    #ncycles_per_iteration=1100,
+    binary_operators=["+", "*", "-", "/", "^"], # Defino os operadores binários que vou usar (já tem os mais importantes)
+    unary_operators=[ # Defino os operadores unários que vou usar (já tem os mais importantes)
+        "sin",
+        "cos",
+        "exp",
+        "log",
+        "sinh",
+        "cosh",
+        "erf",
+    ],
+    turbo=True, # Tende a acelerar o processo de treinamento, mas pode gerar erros em alguns casos
+    bumper=True, # Mesmo funcionamento que o turbo
+    #batching=True,
+    warm_start=True, # Deixar sempre true para ter a possibilidade de continuar de onde parou
+    nested_constraints={"sin": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1}, "cos": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1},
+                        "sinh": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1}, "cosh": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1},
+                        "erf": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1}, "log": {"sin": 1, "cos": 1, "sinh": 1, "cosh": 1, "erf": 1, "log": 1}},
+    constraints={"^": (9, 1)},
+    annealing=True,
+    progress=False, # Desativo a barra de progresso (Eu acho feio ¯\_(ツ)_/¯)
+    verbosity=1, # Defina como 1 para mostrar todas as equações na hora do treinamento
+    run_id="resultados", # Caminho para onde vai o modelo treinado
+)
+modelo.tournament_selection_n = int(modelo.population_size*0.303) # Default = 15 /tamanho do torneio
+modelo.topn = int(modelo.population_size*0.303) # Default = 12 / elitismo
+
+iteracoes = modelo.niterations
+
+for i in range(1,7):
+    modelo.fit(X=var_independentes, y=var_dependentes)#, variable_names=nome_var_independentes)
+    
+    with open("mse_evolution/dados.txt", "a") as f:
+        f.write(f"{iteracoes},{mean_squared_error(var_dep, modelo.predict(var_ind))}\n")
+    modelo.niterations = 10**i - iteracoes
+    iteracoes = 10**i
+
+print(modelo)
